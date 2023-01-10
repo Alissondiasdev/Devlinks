e698e9f8c84ad1553dcaa0b667d7ece6c7f95b31
alisson dias <alissondias120894@gmail.com>
2023-01-10 12:53:59 -0300

    projeto dev links concluído

---
+++ index.js
@@ -0,0 +1,9 @@
+const toggle  = () =>{
+    const html = document.getElementById("html")
+    if(html.classList.contains('ligth')){
+        html.classList.remove('ligth')
+    }else{
+        html.classList.add('ligth')
+    }
+
+}
